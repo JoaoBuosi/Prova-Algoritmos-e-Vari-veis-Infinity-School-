686e666d78d32bec641ffe2fb62eda637a63be15
JoaoBuosi <joaovbuosi@gmail.com>
2025-03-22 18:29:11 -0300

    Update licao aula 1.py

--- licao aula 1.py
+++ licao aula 1.py
@@ -1,10 +1,11 @@
 print("Hello world")
 numero=(input("digite um numero"))
 
-usuario=(input("Digite o seu nome"))
-idade=(input("digite a sua idade"))
-altura=(input("digite a sua altura"))
-cpf=(input("digite o seu cpf"))
+usuario=str(input("Digite o seu nome"))
+idade=int(input("digite a sua idade"))
+altura=float(input("digite a sua altura"))
+cpf=str (input("digite o seu cpf"))
 
 print(f"o numero informado foi {numero}")
-print(f"Bem vindo {usuario}, sua idade é {idade} anos, sua altura é {altura}m seu cpf é {cpf}")
+print(f"Bem vindo {usuario}, sua idade é {idade} anos, sua altura é {altura:.2f}m seu cpf é {cpf}")
+
